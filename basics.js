54b9064e7752d12c629952181c4822251900a03b
Nichollette Acosta <nicholletteacosta@gmail.com>
2021-06-11 22:45:16 -0400

    data strutures javascript

---
+++ basics.js
@@ -0,0 +1,20 @@
+console.log("Bonjour madame");
+var myNumber = 3;
+var myString = "I am cute"
+var myBoolean = true
+var myArray = []
+var myObject = {}
+
+var undefinedVar;
+console.log(undefinedVar)
+
+var nullVar = null;
+console.log(nullVar);
+
+var animalsArray = ['cat', 'dog']
+var theSameAnimalsArray = new Array('cat', 'dog')
+console.log(animalsArray);
+console.log(theSameAnimalsArray);
+console.log(animalsArray[1])
+animalsArray[2]='pig'
+console.log(animalsArray);
